3d814ab794efc9d60725cc7935d4280edf665e18
Daniel Carvalho <danielcfleite@gmail.com>
2023-12-05 15:21:40 -0300

    production-config

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,10 @@ import { default as knex, Knex } from "knex";
 import { env } from "./env";
 
 export const config = { 
-    client: 'sqlite',
-    connection : {
+    client: env.DATABASE_CLIENT,
+    connection : env.DATABASE_CLIENT === 'sqlite' ? {
         filename: env.DATABASE_URL,
-    },
+    } : env.DATABASE_URL,
     useNullAsDefault:true,
     migrations: {
         extension: "ts",
